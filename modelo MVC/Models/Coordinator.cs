7818ae70b743678084fc430814bea0e919c5f857
nazbrey <nazbrey@hotmail.com>
2022-09-04 10:59:32 -0500

    Actualizaro Codigo
    Se actualiza solo coordinador

--- modelo MVC/Models/Coordinator.cs
+++ modelo MVC/Models/Coordinator.cs
@@ -9,14 +9,20 @@ namespace modelo_MVC.Models
     internal class Coordinator : Entity
     {
         public string Name { get; private set; }
-       
         public string Description { get; private set; }
-        public List<Enterprise> Enterprises { get; private set; }
-        public Coordinator(int id, string name, Enterprise enterprise ) : base(id)
+        public List<CoordinatorWork> coordinatorWork { get; private set; }
+
+        public Coordinator(int id, string name) : base(id)
         { 
             this.Name = name;
-            Enterprise = enterprise;
-            this.Description = name;    
+            this.Description = name;
+            coordinatorWork = new();
+        }
+        public void AddWork(string workId) 
+        {
+            this.coordinatorWork.Add(
+                new CoordinatorWork(this.Id,Guid.NewGuid().ToString(), workId));
+        
         }
     }
 }
